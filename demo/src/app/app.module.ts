e06147d4558c9d25199b551aa62e5ef804a9a12b
Carlos Andrés <carlos.andres.aguirre.c@gmail.com>
2018-06-16 14:49:04 -0500

    Segunda entrega de programa base

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -4,123 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ButtonModule} from 'primeng/button';
 import {MenuModule} from 'primeng/menu';
-import {MenuItem} from 'primeng/api';
 import {PanelMenuModule} from 'primeng/panelmenu';
+import { PanelModule } from 'primeng/components/panel/panel';
+import { RadioButtonModule } from 'primeng/components/radioButton/radioButton';
 
-//import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {Menubar,MenuItem} from 'primeng/primeng';
+import {PanelMenu} from './menu/panelmenu.component'
+
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelMenu
   ],
   imports: [
     BrowserModule,
     ButtonModule,
     MenuModule,
     //MenuItem,
-    PanelMenuModule
-   // AccordionModule
+    PanelMenuModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    PanelModule,
+    RadioButtonModule
     ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-export class MenuDemo {
-
-    private items: MenuItem[];
-
-    ngOnInit() {
-        this.items = [{
-            label: 'File',
-            items: [
-                {label: 'New', icon: 'fa fa-plus'},
-                {label: 'Open', icon: 'fa fa-download'}
-            ]
-        },
-        {
-            label: 'Edit',
-            items: [
-                {label: 'Undo', icon: 'fa fa-refresh'},
-                {label: 'Redo', icon: 'fa fa-repeat'}
-            ]
-        }];
-    }
-}
-
-export class PanelMenuDemo {
-  
-    items: MenuItem[];
-
-    ngOnInit() {
-        this.items = [
-            {
-                label: 'File',
-                icon: 'fa fa-fw fa-file-o',
-                items: [{
-                        label: 'New', 
-                        icon: 'fa fa-fw fa-plus',
-                        items: [
-                            {label: 'Project'},
-                            {label: 'Other'},
-                        ]
-                    },
-                    {label: 'Open'},
-                    {label: 'Quit'}
-                ]
-            },
-            {
-                label: 'Edit',
-                icon: 'fa fa-fw fa-edit',
-                items: [
-                    {label: 'Undo', icon: 'fa fa-fw fa-mail-forward'},
-                    {label: 'Redo', icon: 'fa fa-fw fa-mail-reply'}
-                ]
-            },
-            {
-                label: 'Help',
-                icon: 'fa fa-fw fa-question',
-                items: [
-                    {
-                        label: 'Contents'
-                    },
-                    {
-                        label: 'Search', 
-                        icon: 'fa fa-fw fa-search', 
-                        items: [
-                            {
-                                label: 'Text', 
-                                items: [
-                                    {
-                                        label: 'Workspace'
-                                    }
-                                ]
-                            },
-                            {
-                                label: 'File'
-                            }
-                    ]}
-                ]
-            },
-            {
-                label: 'Actions',
-                icon: 'fa fa-fw fa-gear',
-                items: [
-                    {
-                        label: 'Edit',
-                        icon: 'fa fa-fw fa-refresh',
-                        items: [
-                            {label: 'Save', icon: 'fa fa-fw fa-save'},
-                            {label: 'Update', icon: 'fa fa-fw fa-save'},
-                        ]
-                    },
-                    {
-                        label: 'Other',
-                        icon: 'fa fa-fw fa-phone',
-                        items: [
-                            {label: 'Delete', icon: 'fa fa-fw fa-minus'}
-                        ]
-                    }
-                ]
-            }
-        ];
-    }
-}
---
+++ demo/src/app/menu/panelmenu.component.ts
@@ -0,0 +1,94 @@
+import {Component} from '@angular/core';
+import {PanelMenuModule} from 'primeng/panelmenu';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+    selector:'menu',
+    template: `
+        <h1>{{nombre}}</h1> 
+        <p-panelMenu [model]="items" [style]="{'width':'300px'}"></p-panelMenu>
+        `
+})
+
+export class PanelMenu {
+    public nombre='Menú LMS';
+  
+    items: MenuItem[];
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'File',
+                icon: 'fa fa-fw fa-file-o',
+                items: [{
+                        label: 'New', 
+                        icon: 'fa fa-fw fa-plus',
+                        items: [
+                            {label: 'Project', icon: 'fa fa-fw fa-lock'},
+                            {label: 'Other', icon: 'fa fa-fw fa-list'}
+                        ]
+                    },
+                    {label: 'Open', icon: 'fa fa-fw fa-external-link'},
+                    {separator: true},
+                    {label: 'Quit', icon: 'fa fa-fw fa-close'}
+                ]
+            },
+            {
+                label: 'Edit',
+                icon: 'fa fa-fw fa-edit',
+                items: [
+                    {label: 'Undo', icon: 'fa fa-fw fa-mail-forward'},
+                    {label: 'Redo', icon: 'fa fa-fw fa-mail-reply'}
+                ]
+            },
+            {
+                label: 'Help',
+                icon: 'fa fa-fw fa-question',
+                items: [
+                    {
+                        label: 'Contents',
+                        icon: 'fa fa-fw fa-bars'
+                    },
+                    {
+                        label: 'Search', 
+                        icon: 'fa fa-fw fa-search', 
+                        items: [
+                            {
+                                label: 'Text', 
+                                items: [
+                                    {
+                                        label: 'Workspace'
+                                    }
+                                ]
+                            },
+                            {
+                                label: 'File',
+                                icon: 'fa fa-fw fa-file',
+                            }
+                    ]}
+                ]
+            },
+            {
+                label: 'Actions',
+                icon: 'fa fa-fw fa-gear',
+                items: [
+                    {
+                        label: 'Edit',
+                        icon: 'fa fa-fw fa-refresh',
+                        items: [
+                            {label: 'Save', icon: 'fa fa-fw fa-save'},
+                            {label: 'Update', icon: 'fa fa-fw fa-save'},
+                        ]
+                    },
+                    {
+                        label: 'Other',
+                        icon: 'fa fa-fw fa-phone',
+                        items: [
+                            {label: 'Delete', icon: 'fa fa-fw fa-minus'}
+                        ]
+                    }
+                ]
+            }
+        ];
+    }
+}
